4db0090540250590e31ebc484e0463cd9c8a0bb8
njlordi <njlordi@gmail.com>
2016-05-08 23:19:04 -0400

    Initial commit

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+	public Vector3 StartingPoint;
+	public float speed;
+	
+	void OnEnable () {
+		StartingPoint = transform.position;
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(-Vector3.forward * (Time.deltaTime * speed));
+	}
+	
+	void OnDisable() {
+		transform.position = StartingPoint;
+	}
+}
---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundSpawner : MonoBehaviour {
+	
+	public GameObject [] groundVariants; 
+	public float groundSpeed;
+	
+	void Start () {
+		groundSpeed = 4.0f;
+		if (groundVariants != null){
+			Debug.Log("Setting speed for each ground in array");
+			foreach (GameObject go in groundVariants) {
+				go.GetComponent<Ground>().speed = groundSpeed;
+				Debug.Log(go.transform.position.z);
+			}
+		}
+	}
+		
+	void Update () {
+		foreach (GameObject go in groundVariants) {
+				// disable ground piece when it reaches a certain z coordinate (IE not visable anymore!)
+			if (go.transform.position.z <= -10) {
+				go.SetActive(false);
+			}
+			//go.GetComponent<Ground>().speed = groundSpeed;
+			Debug.Log(go.transform.position.z);
+		}
+	}
+}
