7a4f8fd8b9537921ec1e862993c29435acde12f6
njlordi <njlordi@gmail.com>
2016-05-19 23:06:41 -0400

    Almost completely reprogrammed the game, player now physically turns instead of the world turning around him.

---
+++ Assets/Scripts/ObsoleteScripts/OldGroundPiece.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OldGroundPiece : MonoBehaviour {
+	
+	public Vector3 startingPoint;
+	
+	public enum GroundPieces {start, straight, right, left, cross};
+	public GroundPieces gp;
+	
+	public static float speed = 20;
+	public float turnAngle;
+	public float turnSpeed;
+	// for flagging
+	//public bool hasRotated = false;
+
+	void Awake() {
+		// this number depends on type of piece(right/left)
+		switch (gp) {
+		case GroundPieces.right:
+			turnAngle = 360f;
+			break;
+		case GroundPieces.left:
+			turnAngle = 180f;
+			break;
+		default:
+			break;
+		}
+	}
+	
+	void OnEnable () {
+		startingPoint = new Vector3 (0, 1.0f, 40.0f);
+	}	
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		// main movement of platform
+		transform.position -= new Vector3 (0, 0, Time.deltaTime * speed);
+		
+		turnSpeed = speed * Mathf.PI;
+		
+		if (gp != GroundPieces.straight) {
+			RotatePiece();
+		}
+		
+		if (transform.position.z <= -100.0f) {
+			if (gp == GroundPieces.start) {
+				Destroy(gameObject);
+			} else {
+				gameObject.SetActive(false);
+			}
+		}
+	}
+	
+	void RotatePiece() {
+		Debug.Log("RotatePiece Called...");
+		
+		// once the ground is out of bounds and not rotated to 270 degrees, start rotating
+		// replace this code with Mathf.clamp?
+		
+		switch(gp) {
+		case GroundPieces.right:
+		if (transform.position.z <= 15 && turnAngle >= 270f) {
+			turnAngle -= Time.deltaTime * turnSpeed;
+			transform.rotation = Quaternion.Euler(0.0f, turnAngle, 0.0f);
+		/* if the ground is out of bounds and has reached it's rotation goal set precise angle of 270 
+		(this may not be necessary) */	
+		} else if (transform.position.z <= 15) {
+			transform.rotation = Quaternion.Euler(0.0f, 270f, 0.0f);
+		}
+			break;
+		case GroundPieces.left:
+			if (transform.position.z <= 15 && turnAngle <= 270f) {
+				turnAngle += Time.deltaTime * turnSpeed;
+				transform.rotation = Quaternion.Euler(0.0f, turnAngle, 0.0f);
+		/* if the ground is out of bounds and has reached it's rotation goal set precise angle of 270 
+		(this may not be necessary) */	
+			} else if (transform.position.z <= 15) {
+				transform.rotation = Quaternion.Euler(0.0f, 270f, 0.0f);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotation : MonoBehaviour {
+	
+	public string directionToTurn;
+	public float turnSpeed;
+	public float currentYRotation;
+	
+	public Quaternion targetRotation;
+	public float degreesToTurn;
+	
+	public bool keyInputEnabled;
+	public bool flagForDegreeChange;
+
+	// Use this for initialization
+	void Awake () {
+		degreesToTurn = 0.0f;
+		turnSpeed = 200.0f;
+		targetRotation = Quaternion.identity;
+	}
+	
+	void Start() {
+		flagForDegreeChange = true;
+		keyInputEnabled = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		currentYRotation = gameObject.transform.rotation.eulerAngles.y;
+		
+		if (Input.GetKey(KeyCode.LeftArrow) && keyInputEnabled) {
+			directionToTurn = "left";
+			keyInputEnabled = false;
+			flagForDegreeChange = true;
+		}
+		if (Input.GetKey(KeyCode.RightArrow) && keyInputEnabled) {
+			directionToTurn = "right";
+			keyInputEnabled = false;
+			flagForDegreeChange = true;
+		}
+		
+		switch(directionToTurn){
+
+		case "left":
+			TurnLeft();
+			break;			
+		case "right":
+			TurnRight();
+			break;
+		default:
+			break;
+			
+		}
+		targetRotation = Quaternion.Euler(0.0f, degreesToTurn, 0.0f);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+		
+		if (transform.rotation == targetRotation) {
+			keyInputEnabled = true;
+		}
+	}
+	
+	void TurnLeft() {
+		if (flagForDegreeChange) {
+			degreesToTurn -= 90.0f;
+			flagForDegreeChange = false;
+		}
+		//targetRotation = Quaternion.Euler(0.0f, degreesToTurn, 0.0f);
+	}
+	void TurnRight() {
+		if (flagForDegreeChange) {
+			degreesToTurn += 90.0f;
+			flagForDegreeChange = false;
+		}
+		//targetRotation = Quaternion.Euler(0.0f, degreesToTurn, 0.0f);
+	}
+}
