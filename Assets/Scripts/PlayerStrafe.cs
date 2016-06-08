d1a33abbb0f424df07d953e3235a0ba76d3aa7e2
Nicholas Lordi <njlordi@gmail.com>
2016-06-07 22:37:05 -0400

    Minor changes to player strafe code.

--- Assets/Scripts/PlayerStrafe.cs
+++ Assets/Scripts/PlayerStrafe.cs
@@ -9,10 +9,18 @@ public class PlayerStrafe : MonoBehaviour {
 	public float strafeFixedFrame;
 	public float strafeDistanceFromOrigin;
 	
+	// rename these next two
+	public bool strafeLeftFlag;
+	public bool strafeRightFlag;
+	
 	public bool inputEnabled;
+	
+	public enum StrafeLocation {left, center, right};
+	public StrafeLocation currentStrafeLocation;
 	
 	// Use this for initialization
 	void Start () {
+		currentStrafeLocation = StrafeLocation.center;
 		inputEnabled = true;
 		strafeAmount = 8.0f;
 		strafeSpeed = 20.0f;
@@ -22,23 +30,53 @@ public class PlayerStrafe : MonoBehaviour {
 	void Update () {
 		gameObject.transform.Translate(Vector3.forward * Time.deltaTime * runSpeed);
 		
+		if (Input.GetKey(KeyCode.A) && inputEnabled == true) {
+			StartCoroutine("strafeLeft");
+		}
 		if (Input.GetKey(KeyCode.D) && inputEnabled == true) {
-			StartCoroutine("moveRight");	
+			StartCoroutine("strafeRight");	
+		}
+		
+		if (playerStrafingFlag == true) {
+			
+			playerStrafingFlag = false;
 		}
 	}
 	
-	IEnumerator moveRight() {
+	// DITCH THE COROUTINES
+	
+	IEnumerator strafeLeft() {
+		Debug.Log("A Key pressed");
+		inputEnabled = false;
+		strafeDistanceFromOrigin = 0;
+		
+		while (strafeDistanceFromOrigin < strafeAmount) {
+			strafeFixedFrame = Time.deltaTime * strafeSpeed;
+			transform.position -= transform.right * strafeFixedFrame;
+			strafeDistanceFromOrigin += strafeFixedFrame;
+			yield return null;
+		}
+		if (currentStrafeLocation == StrafeLocation.center)
+			currentStrafeLocation = StrafeLocation.left;
+		else
+			currentStrafeLocation = StrafeLocation.center;
+		inputEnabled = true;
+	}
+	IEnumerator strafeRight() {
 		Debug.Log("D Key pressed");
 		inputEnabled = false;
 		strafeDistanceFromOrigin = 0;
-
+		
 		while (strafeDistanceFromOrigin < strafeAmount) {
 			strafeFixedFrame = Time.deltaTime * strafeSpeed;
 			transform.position += transform.right * strafeFixedFrame;
 			strafeDistanceFromOrigin += strafeFixedFrame;
 			yield return null;
 		}
-		
+		if (currentStrafeLocation == StrafeLocation.center)
+			currentStrafeLocation = StrafeLocation.right;
+		else
+			currentStrafeLocation = StrafeLocation.center;
 		inputEnabled = true;
 	}
 }
