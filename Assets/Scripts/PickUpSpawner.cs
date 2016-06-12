093e37cbef141acaf7d8619c82e1d1480da770e3
Nicholas Lordi <njlordi@gmail.com>
2016-06-12 02:30:21 -0400

    Added randomized pickup placement.

--- Assets/Scripts/PickUpSpawner.cs
+++ Assets/Scripts/PickUpSpawner.cs
@@ -5,16 +5,23 @@ public class PickUpSpawner : MonoBehaviour {
 	public GameObject coinPickupPrefab;
 	
 	public float laneValue;
+	
+	// outsource this variable to static method later
+	public float pickupSpacing;
+	
 	// Use this for initialization
 	
 	void OnEnable () {
-		laneValue = 0.0f;
+		pickupSpacing = 0.04f;
+		Random.seed = System.DateTime.Now.Millisecond;
+		laneValue = pickupSpacing * Random.Range(-1, 2);
 		
 		
 		for (float i = 0; i < 10; i++) {
 			float currentZLocation = 0.45f - (i * 0.1f);
 			
-			GameObject go = (GameObject) Instantiate (coinPickupPrefab, transform.position, transform.rotation);	
+			GameObject go = (GameObject) Instantiate (coinPickupPrefab, 
+				transform.position, transform.rotation);	
 			go.transform.parent = this.transform;
 			go.transform.localPosition = new Vector3 (laneValue, 2.0f, currentZLocation);
 		}
