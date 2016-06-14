e98068b201da4e495dfd1d3a49cfdb815ed4fed1
njlordi <njlordi@gmail.com>
2016-06-13 21:26:24 -0400

    Added basic functionality for automatic turning.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,43 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	
 	public float runSpeed;
+	public float turnSpeed;
+	Quaternion targetRotation;
+	float degreesToTurn;
+	public bool turnInputEnabled;
 	
-	// Update is called once per frame
+	void Awake () {
+		degreesToTurn = 0.0f;
+		turnSpeed = 200.0f;
+		targetRotation = Quaternion.identity;
+	}
+	
+	void Start() {
+		turnInputEnabled = true;
+		GroundPiece.MoveLeft();
+	}
+	
 	void Update () {
+		turnSpeed = runSpeed * 5.0f;
+		
 		gameObject.transform.Translate(Vector3.forward * Time.deltaTime * runSpeed);
+		
+		targetRotation = Quaternion.Euler(0.0f, degreesToTurn, 0.0f);
+		
+		if (transform.rotation == targetRotation) {
+			turnInputEnabled = true; //remove?
+		} else {
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+		}
+	}
+	
+	public void TurnLeft() {
+		turnInputEnabled = false;
+		degreesToTurn -= 90.0f;
+	}
+	public void TurnRight() {
+		turnInputEnabled = false;
+		degreesToTurn += 90.0f;
 	}
 }
---
+++ Assets/Scripts/TurnTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnTrigger : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.transform.parent.tag == "PlayerGeneralLocation"
+		&& this.transform.parent.tag == "LeftTurnPiece") {
+			other.GetComponentInParent<PlayerMovement>().TurnLeft();
+		}
+		if (other.transform.parent.tag == "PlayerGeneralLocation"
+		&& this.transform.parent.tag == "RightTurnPiece") {
+			other.GetComponentInParent<PlayerMovement>().TurnRight();
+		}
+	}
+}
