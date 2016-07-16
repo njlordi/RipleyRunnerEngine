3fb00252043535e18c1a4a688cf1481c1e14f74d
Nicholas Lordi <njlordi@gmail.com>
2016-07-16 01:18:31 -0400

    Cars now cross intersections by chance.

--- Assets/Scripts/LevelScripts/Car.cs
+++ Assets/Scripts/LevelScripts/Car.cs
@@ -2,17 +2,26 @@
 using System.Collections;
 
 public class Car : MonoBehaviour {
+	
 	public float carSpeed;
 	public float delayInCarMovement;
 	Vector3 carOrigin;
 	float timeMarker;
-	// Use this for initialization
+
+	// scale of 1-5 (1 = will not happen) (5 = guaranteed)
+	float chanceToExist;
 
 	void OnEnable () {
+		chanceToExist = 2;
 		carOrigin = this.transform.localPosition;
-		carSpeed = Random.Range(18.0f, 60.0f);
         delayInCarMovement = Random.Range(1.0f, 2.0f);
 		timeMarker = Time.time;
+
+		if (Random.Range (1, 5) < chanceToExist) {
+			carSpeed = Random.Range (20.0f, 60.0f);
+		} else {
+			carSpeed = 0;
+		}
 	}
 
 	// Update is called once per frame
