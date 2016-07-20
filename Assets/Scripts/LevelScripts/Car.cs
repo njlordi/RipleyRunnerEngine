761f0d72516d8476e3df23ee75ea2bf8e61e02ab
Nicholas Lordi <njlordi@gmail.com>
2016-07-20 01:50:34 -0400

    Car script now has a separate method to determine if it exists at the moment.

--- Assets/Scripts/LevelScripts/Car.cs
+++ Assets/Scripts/LevelScripts/Car.cs
@@ -1,36 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Car : MonoBehaviour {
-	
+public class Car : MonoBehaviour
+{
+	// scale of 1-5 (1 = will not happen) (5 = guaranteed) ...maybe move to centralized class?
+	float chanceToExist;
+	public bool isExistent;
+
 	public float carSpeed;
 	public float delayInCarMovement;
+	Renderer carRenderer;
 	Vector3 carOrigin;
 	float timeMarker;
 
-	// scale of 1-5 (1 = will not happen) (5 = guaranteed)
-	float chanceToExist;
+	void Awake ()
+	{
+		carRenderer = GetComponent<Renderer> ();
+	}
 
-	void OnEnable () {
-		chanceToExist = 2;
+	void OnEnable ()
+	{
+		rollForExistence ();
+
+		chanceToExist = 3;
 		carOrigin = this.transform.localPosition;
-        delayInCarMovement = Random.Range(1.0f, 2.0f);
+		delayInCarMovement = Random.Range (1.0f, 2.0f);
 		timeMarker = Time.time;
 
-		if (Random.Range (1, 5) < chanceToExist) {
+		if (isExistent) {
 			carSpeed = Random.Range (20.0f, 60.0f);
+			carRenderer.enabled = true;
 		} else {
 			carSpeed = 0;
+			carRenderer.enabled = false;
 		}
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		if (Time.time >= (timeMarker + delayInCarMovement))
 			this.transform.Translate (Vector3.forward * Time.deltaTime * carSpeed);
 	}
-		
-	void OnDisable() {
+
+	void OnDisable ()
+	{
 		this.transform.localPosition = carOrigin;
+	}
+
+	void rollForExistence ()
+	{
+		if (Random.Range (1, 5) < chanceToExist) {
+			isExistent = true;
+		} else {
+			isExistent = false;
+		}
+			
 	}
 }
